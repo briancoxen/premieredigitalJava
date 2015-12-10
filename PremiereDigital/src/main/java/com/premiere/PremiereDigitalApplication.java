e71d3a5539b48fd8f8cfea2f13af416e3e7fe4de
Brian <briancoxen@gmail.com>
2015-12-09 21:04:15 -0800

    Initial Commit

---
+++ PremiereDigital/src/main/java/com/premiere/PremiereDigitalApplication.java
@@ -0,0 +1,12 @@
+package com.premiere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremiereDigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PremiereDigitalApplication.class, args);
+    }
+}
