bba69044e24437ae64c396e3435283dfdf0e3025
Brian <briancoxen@gmail.com>
2015-12-13 10:29:37 -0800

    Updated xml parse to new DB

--- PremiereDigital/src/main/java/premiereXML/XMLHandler.java
+++ PremiereDigital/src/main/java/premiereXML/XMLHandler.java
@@ -35,17 +35,25 @@ public class XMLHandler {
 	public String loadXMLData() throws SQLException {
 		NodeList nList = doc.getElementsByTagName("movie");
 		for (int count = 0; count < nList.getLength(); count++) {
+			
 			Node movies = nList.item(count);
 			Element eMovies = (Element) movies;
-			
 			String title = eMovies.getElementsByTagName("Title").item(0).getTextContent();
 			String md5 = eMovies.getElementsByTagName("MD5").item(0).getTextContent();
-			String meta = eMovies.getElementsByTagName("Meta").item(0).getTextContent();
+			
+			NodeList meta = eMovies.getElementsByTagName("Meta").item(0).getChildNodes();
+			Element eMeta = (Element) meta;
+			String director = eMeta.getElementsByTagName("Director").item(0).getTextContent();
+			String type = eMeta.getElementsByTagName("Type").item(0).getTextContent();
+			String description = eMeta.getElementsByTagName("Description").item(0).getTextContent();
+			String length = eMeta.getElementsByTagName("Length").item(0).getTextContent();
+			String release = eMeta.getElementsByTagName("Release").item(0).getTextContent();
+			
 			
 		    PremiereDBConn mySQL = new PremiereDBConn();
 		    Connection myDB = null;
 		    Statement stmt = null;
-		    String update = String.format("INSERT into movies values ('%s','%s','%s')", title, meta, md5);
+		    String update = String.format("INSERT into movies values ('%s','%s','%s')", title, md5, director, release, description, length, type);
 		    
 		    try {
 		    	myDB = (Connection) mySQL.getDB();
